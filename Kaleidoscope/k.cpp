59456dbbc7567f43035205d19d349c44b532e9f8
C.y <yangruihan@vip.qq.com>
2020-09-25 00:26:39 +0800

    add support for user-define binary

--- Kaleidoscope/k.cpp
+++ Kaleidoscope/k.cpp
@@ -55,7 +55,11 @@ enum Token
     tok_else = -8,
 
     tok_for = -9,
-    tok_in = -10
+    tok_in = -10,
+
+    // operators
+    tok_binary = -11,
+    tok_unary = -12
 };
 
 static std::string IdentifierStr; // Filled in if tok_identifier
@@ -90,6 +94,10 @@ static int gettok()
             return tok_for;
         if (IdentifierStr == "in")
             return tok_in;
+        if (IdentifierStr == "binary")
+            return tok_binary;
+        if (IdentifierStr == "unary")
+            return tok_unary;
         return tok_identifier;
     }
 
@@ -200,13 +208,28 @@ namespace
     {
         std::string Name;
         std::vector<std::string> Args;
+        bool IsOperator;
+        unsigned Precedence; // Precedence if a binary op.
 
     public:
-        PrototypeAST(const std::string &Name, std::vector<std::string> Args)
-            : Name(Name), Args(std::move(Args)) {}
+        PrototypeAST(const std::string &Name, std::vector<std::string> Args,
+                     bool IsOperator = false, unsigned Precedence = 0)
+            : Name(Name), Args(std::move(Args)),
+              IsOperator(IsOperator), Precedence(Precedence) {}
 
         Function *codegen();
         const std::string &getName() const { return Name; }
+
+        bool isUnaryOp() const { return IsOperator && Args.size() == 1; }
+        bool isBinaryOp() const { return IsOperator && Args.size() == 2; }
+
+        char getOperatorName() const
+        {
+            assert(isUnaryOp() || isBinaryOp());
+            return Name[Name.size() - 1];
+        }
+
+        unsigned getBinaryPrecedence() const { return Precedence; }
     };
 
     /// FunctionAST - This class represents a function definition itself.
@@ -507,13 +530,46 @@ static std::unique_ptr<ExprAST> ParseExpression()
 
 /// prototype
 ///   ::= id '(' id* ')'
+///   ::= binary LETTER number? (id, id)
 static std::unique_ptr<PrototypeAST> ParsePrototype()
 {
-    if (CurTok != tok_identifier)
+    std::string FnName;
+
+    unsigned Kind = 0; // 0 = identifier, 1 = unary, 2 = binary
+    unsigned BinaryPrecedence = 30;
+
+    switch (CurTok)
+    {
+    default:
         return LogErrorP("Expected function name in prototype");
 
-    std::string FnName = IdentifierStr;
-    getNextToken();
+    case tok_identifier:
+        FnName = IdentifierStr;
+        Kind = 0;
+        getNextToken();
+        break;
+
+    case tok_binary:
+        getNextToken();
+        if (!isascii(CurTok))
+            return LogErrorP("Expected binary operator");
+
+        FnName = "binary";
+        FnName += (char)CurTok;
+        Kind = 2;
+        getNextToken();
+
+        // Read the precedence if present.
+        if (CurTok == tok_number)
+        {
+            if (NumVal < 1 || NumVal > 100)
+                return LogErrorP("Invalid precedence: must be 1.100");
+
+            BinaryPrecedence = (unsigned)NumVal;
+            getNextToken();
+        }
+        break;
+    }
 
     if (CurTok != '(')
         return LogErrorP("Expected '(' in prototype");
@@ -527,7 +583,8 @@ static std::unique_ptr<PrototypeAST> ParsePrototype()
     // success.
     getNextToken(); // eat ')'.
 
-    return std::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
+    return std::make_unique<PrototypeAST>(FnName, std::move(ArgNames),
+                                          Kind != 0, BinaryPrecedence);
 }
 
 /// definition ::= 'def' prototype expression
@@ -631,8 +688,16 @@ Value *BinaryExprAST::codegen()
         // Convert bool 0/1 to double 0.0 or 1.0
         return Builder.CreateUIToFP(L, Type::getDoubleTy(TheContext), "booltmp");
     default:
-        return LogErrorV("invalid binary operator");
+        break;
     }
+
+    // If it wasn't a builtin binary operator, it must be a user defined one. Emit
+    // a call to it.
+    Function *F = getFunction(std::string("binary") + Op);
+    assert(F && "binary operator not found!");
+
+    Value *Ops[2] = {L, R};
+    return Builder.CreateCall(F, Ops, "binop");
 }
 
 Value *CallExprAST::codegen()
@@ -684,6 +749,10 @@ Function *FunctionAST::codegen()
     Function *TheFunction = getFunction(P.getName());
     if (!TheFunction)
         return nullptr;
+
+    // If this is an operator, install it.
+    if (P.isBinaryOp())
+        BinopPrecedence[P.getOperatorName()] = P.getBinaryPrecedence();
 
     // Create a new basic block to start insertion into.
     BasicBlock *BB = BasicBlock::Create(TheContext, "entry", TheFunction);
